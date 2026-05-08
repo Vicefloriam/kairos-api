ae8c63c2b9c5c74d98a78fba48b94873983c5159
Vicefloriam <vicenteflorianfh@gmail.com>
2026-05-07 23:11:21 -0500

    Implement products module and relationships, pending POST fix

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 
 Route::apiResource('categories', CategoryController::class);
+Route::apiResource('products', ProductController::class);
